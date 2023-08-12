5d055b05284d0c51d82bb8c6507bf303ede1aa95
Yahikko <taczuhiro@mail.ru>
2023-08-12 16:22:43 +0300

    MVVM pattern

--- app/src/main/java/com/example/cleanmvvm/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cleanmvvm/presentation/MainActivity.kt
@@ -1,52 +1,44 @@
 package com.example.cleanmvvm.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.cleanmvvm.R
-import com.example.cleanmvvm.data.repository.UserRepositoryImpl
-import com.example.cleanmvvm.data.storage.sharedpref.SharedPrefUserStorage
-import com.example.cleanmvvm.domain.models.SaveUserNameParam
-import com.example.cleanmvvm.domain.models.UserName
-import com.example.cleanmvvm.domain.usecase.GetUserNameUseCase
-import com.example.cleanmvvm.domain.usecase.SaveUserNameUseCase
 
 class MainActivity : AppCompatActivity() {
 
-    private val sharedPrefUserStorage by lazy(LazyThreadSafetyMode.NONE) {
-        SharedPrefUserStorage(applicationContext)
-    }
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
-        UserRepositoryImpl(sharedPrefUserStorage)
-    }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        GetUserNameUseCase(userRepository)
-    }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
-        SaveUserNameUseCase(userRepository)
-    }
+
+    private lateinit var vm: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e("AAA", "Activity created")
+
+        vm = ViewModelProvider(this, MainViewModelFactory(this))
+            .get(MainViewModel::class.java)
 
         val dataTextView: TextView = findViewById(R.id.dataTextView)
         val dataEditText: EditText = findViewById(R.id.dataEditText)
         val sendButton: Button = findViewById(R.id.sendButton)
         val receiveButton: Button = findViewById(R.id.receiveButton)
 
+        vm.resultLive.observe(this) {
+            dataTextView.text = it
+        }
+
         sendButton.setOnClickListener {
-            val data = dataEditText.text.toString()
-            val param = SaveUserNameParam(data)
-            val result = saveUserNameUseCase.execute(param)
-            dataTextView.text = "Save result = $result"
+            val text = dataEditText.text.toString()
+            vm.save(text)
         }
 
         receiveButton.setOnClickListener {
-            val userName: UserName = getUserNameUseCase.execute()
-            dataTextView.text = "${userName.firstName} ${userName.lastName}"
+            vm.load()
         }
     }
 }
---
+++ app/src/main/java/com/example/cleanmvvm/presentation/MainViewModelFactory.kt
@@ -0,0 +1,29 @@
+package com.example.cleanmvvm.presentation
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.cleanmvvm.data.repository.UserRepositoryImpl
+import com.example.cleanmvvm.data.storage.sharedpref.SharedPrefUserStorage
+import com.example.cleanmvvm.domain.usecase.GetUserNameUseCase
+import com.example.cleanmvvm.domain.usecase.SaveUserNameUseCase
+
+class MainViewModelFactory(context: Context) : ViewModelProvider.Factory {
+
+    private val userRepository by lazy(LazyThreadSafetyMode.NONE) {
+        UserRepositoryImpl(userStorage = SharedPrefUserStorage(context = context))
+    }
+    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
+        GetUserNameUseCase(userRepository)
+    }
+    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) {
+        SaveUserNameUseCase(userRepository)
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(
+            getUserNameUseCase = getUserNameUseCase,
+            saveUserNameUseCase = saveUserNameUseCase
+        ) as T
+    }
+}
